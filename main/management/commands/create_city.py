1ab315317b112fa6c86d87c4fa45151d42221977
Saeed-kh19 <saeedkhalili719@gmail.com>
2025-03-04 11:54:46 +0330

    completing the management commands for creating 50 cities and created 50 cities in the database successfully done! :)

--- main/management/commands/create_city.py
+++ main/management/commands/create_city.py
@@ -1,4 +1,4 @@
-import Faker
+from faker import Faker
 from django.core.management import BaseCommand
 
 from main.models import City
@@ -8,4 +8,9 @@ fake = Faker()
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for _ in range(50):
-            city_name =
+            city_name = fake.city()
+            longitude = fake.longitude()
+            latitude = fake.latitude()
+            City.objects.create(city_name = city_name, longitude = longitude, latitude = latitude)
+
+        self.stdout.write(self.style.SUCCESS('Cities created successfully!'))
