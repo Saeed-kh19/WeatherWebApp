4a5269a302602a6c49919d90e084c377c17b3372
Saeed-kh19 <saeedkhalili719@gmail.com>
2025-03-04 11:18:12 +0330

    adding a serializer for city lists and then configuring and connecting the urls.py in the project and the api!!

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,10 @@
 from rest_framework import serializers
 
+from main.models import City
+
 
 class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = '__all__'
+        
--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from api.views import CityListView
+
 urlpatterns = [
-    path('/cities',),
+    path('cities/',CityListView.as_view()),
 ]
