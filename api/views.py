b560f1c35c4297026ba03ec59a64595c915ce5fb
Saeed-kh19 <saeedkhalili719@gmail.com>
2025-03-04 11:13:09 +0330

    installing django_rest_framework and added it to the setting.py in the project folder and creating city list class in the views.py in api folder and then i created and initialized serializers.py in the api

--- api/views.py
+++ api/views.py
@@ -1,2 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
+from main.models import City
+
+
+class CityListView(ListView):
+    model = City
+    context_object_name = 'cities'
+
+    def get(self,request,*args,**kwargs):
+        cities = City.objects.all()
+        context = {'cities': cities}
+        return cities
+
+    
