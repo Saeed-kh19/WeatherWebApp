a63cb048db18da46fff5e28c2c91102885af1537
Saeed-kh19 <saeedkhalili719@gmail.com>
2025-03-04 14:19:58 +0330

    completing views.py in the api and adding a class for CityWeatherInfo in the serializers.py in the api

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 
-from main.models import City
+from main.models import City, CityWeatherInfo
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = '__all__'
-        
+
+class CityWeatherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CityWeatherInfo
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -3,8 +3,8 @@ from django.views.generic import ListView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers import CitySerializer
-from main.models import City
+from api.serializers import CitySerializer, CityWeatherSerializer
+from main.models import City, CityWeatherInfo
 
 
 class CityListView(APIView):
@@ -14,7 +14,8 @@ class CityListView(APIView):
     def get(self,request,*args,**kwargs):
         cities = City.objects.all()
         context = {'cities': cities}
-        return Response(context)
+        serializer = CitySerializer(cities, many=True)
+        return Response(serializer.data)
 
 
 class CityWeatherView(APIView):
@@ -23,7 +24,16 @@ class CityWeatherView(APIView):
             return Response({'Error':'city_name is required!'},status=400)
 
         else:
-            city_name = request.data['city_name']
-            cities = City.objects.filter(city_name=city_name)
-            serializer_cities = CitySerializer(cities, many=True)
-            return Response({'cities': serializer_cities.data})
+            city_name = (request.data['city_name']).lower()
+            city = City.objects.filter(city_name=city_name).first()
+            if not city:
+                return Response({'Error':'city_name does not exist!'},status=400)
+            else:
+                city_weather = CityWeatherInfo.objects.filter(city=city).order_by('-date_submitted').first()
+                if not city_weather:
+                    return Response({'Error':'No weather data for this city!'},status=404)
+                else:
+                    city_serializer = CitySerializer(city)
+                    city_weather_serializer = CityWeatherSerializer(city_weather)
+
+                    return Response({'city':city_serializer.data, 'lastest_weather':city_weather_serializer.data})
